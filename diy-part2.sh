ff5864760c9492f1b3c8e996adacf3815a76a47a
getenjr <32555548+getenjr@users.noreply.github.com>
2025-02-01 08:03:11 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,5 +19,5 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-sed -i 's/192.168.1.1/192.168.86.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 bash feeds/packages/net/chinadns-ng/update.sh
